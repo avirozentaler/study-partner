7c162057ab393a793fb85ee65324be99d2f8e63d
student <student@BA.local>
2022-10-27 22:30:59 +0300

    add log-in

--- client/src/componets/authentication/Authentication.jsx
+++ client/src/componets/authentication/Authentication.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import {BrowserRouter, Route, Routes} from "react-router-dom"
 import Register from "./register/Register";
-// import Login from "./logIn/LogIn";
+import Login from "./logIn/LogIn";
+import Home from "../home/index"
+import ResetPassword from "./resetPassword/ResetPassword"
 import '../authentication/authentication.css'
 export default function Authentication (){
     
@@ -12,8 +14,12 @@ export default function Authentication (){
     <div  className="Authentication">
             <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Register/>}/>
-                {/* <Route path="/" element={<Login/>}/> */}
+                <Route path="/register" element={<Register/>}/>
+                <Route path="/log-in" element={<Login/>}/>
+                <Route path="/home" element={<Home/>}/>
+                <Route path="/resetPassword" element={<ResetPassword/>}/>
+                
+
 
 
                 
