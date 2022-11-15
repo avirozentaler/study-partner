14339083f080dc0f0243a74e5c1cb4164adbb1d9
Avi <avirozentaler@gmail.com>
2022-11-15 18:12:17 +0200

    added model db for subject

---
+++ server/models/subject.js
@@ -0,0 +1,30 @@
+const db = require('../db/mysql');
+const { DataTypes } = require('sequelize');
+
+const subject = db.define('subject', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+
+    },
+
+    category_id: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+    ,
+    user_connected: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+},
+    {
+        timestamps: false
+    }
+);
+
+(async () => {
+    await db.sync();
+})()
+
+module.exports = subject
