0dcc96510ea3ea5c7e84595de3e177c3157a5c73
Yizhak Shenk <1997shenk@gmail.com>
2022-12-06 17:34:40 +0200

    add buton post

--- client/src/componets/home/bodyApp/BodyApp.jsx
+++ client/src/componets/home/bodyApp/BodyApp.jsx
@@ -1,82 +1,82 @@
-import React, { useContext } from 'react'
-import UserConnected from '../../../context/UserConnected';
-import Auth from '../../authentication/auth/Auth';
-import { Box, Typography, IconButton, Button }
-    from '@mui/material'
-import CloseIcon from '@mui/icons-material/Close';
-import { useColorScheme, styled } from '@mui/material/styles';
-import PropTypes from 'prop-types';
-import Dialog from '@mui/material/Dialog';
-import DialogTitle from '@mui/material/DialogTitle';
-import DialogContent from '@mui/material/DialogContent';
-import DialogActions from '@mui/material/DialogActions';
+import React, { useContext } from "react";
+import UserConnected from "../../../context/UserConnected";
+import Auth from "../../authentication/auth/Auth";
+import {DialogActions, DialogContent, DialogTitle, Dialog, Fab, Box, Typography, IconButton, Button} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add'; 
+import CloseIcon from "@mui/icons-material/Close";
+import { useColorScheme, styled } from "@mui/material/styles";
+import PropTypes from "prop-types";
+
 
 export default function BodyApp({ openLogIn, handleCloseLogIn }) {
-    const { userConnected, setUserConnected } = useContext(UserConnected);
+  const { userConnected, setUserConnected } = useContext(UserConnected);
 
-    const aaa = () => {
-        setUserConnected({ name: 'amit' })
-    }
-    const BootstrapDialog = styled(Dialog)(({ theme }) => ({
-        '& .MuiDialogContent-root': {
-            padding: theme.spacing(2),
-        },
-        '& .MuiDialogActions-root': {
-            padding: theme.spacing(1),
-        },
-    }));
-    function BootstrapDialogTitle(props) {
-        const { children, onClose, ...other } = props;
-
-        return (
-            <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
-                {children}
-                {onClose ? (
-                    <IconButton
-                        aria-label="close"
-                        onClick={onClose}
-                        sx={{
-                            position: 'absolute',
-                            right: 8,
-                            top: 8,
-                            color: (theme) => theme.palette.grey[500],
-                        }}
-                    >
-                        <CloseIcon />
-                    </IconButton>
-                ) : null}
-            </DialogTitle>
-        );
-    }
-    BootstrapDialogTitle.propTypes = {
-        children: PropTypes.node,
-        onClose: PropTypes.func.isRequired,
-    };
+  const aaa = () => {
+    setUserConnected({ name: "amit" });
+  };
+  const BootstrapDialog = styled(Dialog)(({ theme }) => ({
+    "& .MuiDialogContent-root": {
+      padding: theme.spacing(2),
+    },
+    "& .MuiDialogActions-root": {
+      padding: theme.spacing(1),
+    },
+  }));
+  function BootstrapDialogTitle(props) {
+    const { children, onClose, ...other } = props;
 
     return (
-        <Box>
-            <></>
-            <BootstrapDialog
-                onClose={handleCloseLogIn}
-                aria-labelledby="customized-dialog-title"
-                open={openLogIn}
-            >
-                <BootstrapDialogTitle id="customized-dialog-title" onClose={handleCloseLogIn}>
-               
-                </BootstrapDialogTitle>
-                <DialogContent >
-                    <Auth handleCloseLogIn={handleCloseLogIn}/>
-                </DialogContent>
-                <DialogActions>
+      <DialogTitle sx={{ m: 0, p: 2 }} {...other}>
+        {children}
+        {onClose ? (
+          <IconButton
+            aria-label="close"
+            onClick={onClose}
+            sx={{
+              position: "absolute",
+              right: 8,
+              top: 8,
+              color: (theme) => theme.palette.grey[500],
+            }}
+          >
+            <CloseIcon />
+          </IconButton>
+        ) : null}
+      </DialogTitle>
+    );
+  }
+  BootstrapDialogTitle.propTypes = {
+    children: PropTypes.node,
+    onClose: PropTypes.func.isRequired,
+  };
 
-                </DialogActions>
-            </BootstrapDialog>
-            {userConnected ?
-                <Typography variant='h3'>user  Connected</Typography>
-                :
-                <Typography variant='h3'>user not Connected</Typography>
-            }
-            <Button onClick={aaa}>test connected</Button>
-        </Box>
-    )
+  return (
+    <Box>
+      <></>
+      <BootstrapDialog
+        onClose={handleCloseLogIn}
+        aria-labelledby="customized-dialog-title"
+        open={openLogIn}
+      >
+        <BootstrapDialogTitle
+          id="customized-dialog-title"
+          onClose={handleCloseLogIn}
+        ></BootstrapDialogTitle>
+        <DialogContent>
+          <Auth handleCloseLogIn={handleCloseLogIn} />
+        </DialogContent>
+        <DialogActions></DialogActions>
+      </BootstrapDialog>
+      {userConnected ? (
+        <Typography variant="h3">user Connected</Typography>
+      ) : (
+        <Typography variant="h3">user not Connected</Typography>
+      )}
+      <Button onClick={aaa}>test connected</Button>
+      <Fab variant="extended" color="primary" aria-label="add">
+        <AddIcon />
+        post
+      </Fab>
+    </Box>
+  );
 }
