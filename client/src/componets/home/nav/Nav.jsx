d5c503b5bbb083e6d2adc17f10bcaeaa9faa14fd
Avi <avirozentaler@gmail.com>
2022-12-08 18:56:09 +0200

    handle client log out

--- client/src/componets/home/nav/Nav.jsx
+++ client/src/componets/home/nav/Nav.jsx
@@ -36,7 +36,11 @@ export default function Nav({ setOpenLogIn, setPageVale, pageVale }) {
         setAnchorUserMenu(null);
     };
     const handleLogOut = () => {
+        ((name)=> {
+            document.cookie = name + '=; expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+          })('token')
         setUserConnected(null);
+        
     }
     const handleBodyValContent = () => {
         setPageVale((pageVale) => !pageVale)
